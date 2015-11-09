2cdc538a6e39bcd23269b0c80cd3d8d732fe59e6
Dana Hazen <danahazen79@gmail.com>
2015-11-09 10:30:21 -0600

    initial commit

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/Comments';
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/comments';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({expanded: true}));
