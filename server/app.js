000fef4165f1937b3025a979bc91123cb435374b
Dana Hazen <danahazen79@gmail.com>
2015-12-03 16:26:53 -0600

    photo upload not yet working

--- server/app.js
+++ server/app.js
@@ -7,14 +7,19 @@ var bodyParser = require('body-parser');
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/jewelry_tracker';
 
+var multer = require('multer');
 var cloudinary = require('cloudinary');
 
-cloudinary.config({
-    cloud_name: '',
-    api_key: '',
-    api_secret: ''
+var uploaded = multer({
+    dest: '/tmp/',
+    inMemory: true
 });
 
+cloudinary.config({
+    cloud_name: 'lulvhzmas',
+    api_key: '635366613135358',
+    api_secret: 'pIuek0AkKJ6nnEyxU3uqNtUQvrg'
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({expanded: true}));
@@ -23,9 +28,12 @@ app.use(bodyParser.urlencoded({expanded: true}));
 app.post('/data', function(req,res){
     // pull the data off of the request
     var newProduct = {
+        "name": req.body.nameAdd,
+        "url": req.body.url,
         "type": req.body.typeAdd,
         "style": req.body.styleAdd,
         "color": req.body.colorAdd,
+        "stone": req.body.stoneAdd,
         "cost": req.body.currency,
         "details": req.body.detailsAdd
     };
@@ -34,8 +42,8 @@ app.post('/data', function(req,res){
     pg.connect(connectionString, function (err, client) {
         //SQL Query > Insert Data
         //Uses prepared statements, the $1 and $2 are placeholder variables. PSQL then makes sure they are relatively safe values and then uses them when it executes the query.
-        client.query("INSERT INTO products (type, style, color, cost, details) VALUES ($1, $2, $3, $4, $5) RETURNING id",  //this statement works in postgresql
-            [newProduct.type, newProduct.style, newProduct.color, newProduct.cost, newProduct.details],
+        client.query("INSERT INTO products (name, url, type, style, color, stone, cost, details) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id",  //this statement works in postgresql
+            [newProduct.name, newProduct.url, newProduct.type, newProduct.style, newProduct.color, newProduct.stone, newProduct.cost, newProduct.details],
             function(err, result) {
                 if(err) {
                     console.log("Error inserting data: ", err);
@@ -47,11 +55,15 @@ app.post('/data', function(req,res){
     });
 });
 
-app.post('/url', uploaded.single('file'), function(req,res){
+//Assign URL from cloudinary data to the database
+app.post('/url', uploaded.single('item'), function(req,res){
     console.log(req.file);
-    cloudinary.uploader.upload(req.file.path, function(result){
-        console.log(result);
-        res.send(result)
+    cloudinary.uploader.upload(res.file.path, function(res){
+        console.log(res, 'posted');
+        return url({
+            url: '../upload/url',
+            data: {file: file}
+        })
     });
 });
 
--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     //Click on submit in form will call addProduct
-    $("#addDetails").submit(addProduct);
+    $("#addDetails").submit(addProduct, upload);
 
     //Click on delete button on previous item will call deleteProduct
     $("#addProducts").on('click', '.delete', deleteProduct);
@@ -20,6 +20,29 @@ function getData(){
             console.log(data);
         }
     });
+}
+
+
+//To upload photo to cloudinary and return the URL for database
+function upload(){
+    $.ajax({
+        type: "POST",
+        url: "/url",
+        data: url,
+        success: function(data){
+            item = {};
+
+            console.log(data, 'uploaded');
+            console.log('item before insertion', item);
+            item.url = data.secure_url;
+            console.log('item after insertion', item);
+
+            getData(data);
+
+
+        }
+    });
+
 }
 
 //To add a new product to the site and database
@@ -67,11 +90,13 @@ function updateDOM(data){
 
     for(var i = 0; i < data.length; i++){
         var el = "<div data-id='" + data[i].id + "' class='id well col-md-3' >" +
+            "<p>Name: " + data[i].name + "</p>" +
             "<p>ID #: " + data[i].id + "</p>" +
-            "<img src='./assets/photos/sterling-bracelet.jpg Michael Walters' alt='Sterling Bracelet' title='Sterling Bracelet' height='100' width='100' />" +
+            "<img src='http://res.cloudinary.com/lulvhzmas/image/upload/v1449075535/sample.jpg' alt='" + data[i].name + "' title='" + data[i].name + "' height='150' width='150' />" +
             "<p>Type: " + data[i].type + "</p>" +
             "<p>Style: " + data[i].style + "</p>" +
             "<p>Color: " + data[i].color + "</p>" +
+            "<p>Stone: " + data[i].stone + "</p>" +
             "<p>Cost: " + data[i].cost + "</p>" +
             "<p>Additional Details: " + data[i].details + "</p>" +
             "<button class='delete btn btn-danger' data-id='" +
